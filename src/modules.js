907370986a6424ec3415c32391fa5257c079b8eb
Светлана <autizm6kola@yandex.com>
2025-10-07 19:15:03 +0300

    Новый адрес

--- src/modules.js
+++ src/modules.js
@@ -11,11 +11,11 @@
 // ];
 
 export const modules = [
-  { id: 1, name: "Задачи 1", url: "https://autizm6kola-art.github.io/math_2/" },
-  { id: 2, name: "Шрек", url: "https://autizm6kola-art.github.io/shrek1/" },
-  { id: 3, name: "Урфин", url: "https://autizm6kola-art.github.io/urfinTRI/" },
-  { id: 4, name: "Гудвин", url: "https://autizm6kola-art.github.io/gudvin/" },
-  { id: 5, name: "Звёздная пыль", url: "https://autizm6kola-art.github.io/z-pil/" },
+  { id: 1, name: "ШРЕК", url: "https://autizm6kola-art.github.io/z-pil/" },
+  { id: 2, name: "Урфин", url: "https://autizm6kola-art.github.io/urfinTRI/" },
+  { id: 3, name: "Гудвин", url: "https://autizm6kola-art.github.io/gudvin/" },
+  { id: 4, name: "Шрек", url: "https://autizm6kola-art.github.io/shrek1/" },
+  { id: 5, name: "Задачи 1", url: "https://autizm6kola-art.github.io/zadachi1/" },
   { id: 6, name: "Истории", url: "https://autizm6kola-art.github.io/istorii/" },
   { id: 7, name: "Задачи 2", url: "https://autizm6kola-art.github.io/math_1/" },
   { id: 8, name: "Коралина", url: "https://autizm6kola-art.github.io/coralina/" },

37f7ba0bd9567503e8570e688c591bb07eb5b422
Светлана <autizm6kola@yandex.com>
2025-11-27 06:24:37 +0300

    Почему?

--- src/modules.js
+++ src/modules.js
@@ -12,13 +12,14 @@
 
 export const modules = [
   { id: 1, name: "Шрек", url: "https://autizm6kola-art.github.io/shrek1/" },
-  { id: 2, name: "ШРЕК", url: "https://autizm6kola-art.github.io/z-pil/" },
-  { id: 3, name: "Панда", url: "https://autizm6kola-art.github.io/cht_panda/" },
-  { id: 4, name: "Задачи 1", url: "https://autizm6kola-art.github.io/zadachi1/" },
-  { id: 5, name: "Истории", url: "https://autizm6kola-art.github.io/istorii/" },
-  { id: 6, name: "Задачи 2", url: "https://autizm6kola-art.github.io/math_1/" },
-  { id: 7, name: "Коралина", url: "https://autizm6kola-art.github.io/coralina2" },
-  { id: 8, name: "ГАРРИ", url: "https://autizm6kola-art.github.io/garry_p" },
+  { id: 2, name: "Почему?", url: "https://autizm6kola-art.github.io/istorii_pochemu/" },
+  { id: 3, name: "ШРЕК", url: "https://autizm6kola-art.github.io/z-pil/" },
+  { id: 4, name: "Панда", url: "https://autizm6kola-art.github.io/cht_panda/" },
+  { id: 5, name: "Задачи 1", url: "https://autizm6kola-art.github.io/zadachi1/" },
+  { id: 6, name: "Истории", url: "https://autizm6kola-art.github.io/istorii/" },
+  { id: 7, name: "Задачи 2", url: "https://autizm6kola-art.github.io/math_1/" },
+  { id: 8, name: "Коралина", url: "https://autizm6kola-art.github.io/coralina2" },
+  { id: 9, name: "ГАРРИ", url: "https://autizm6kola-art.github.io/garry_p" },
   
   // { id: 5, name: "Урфин", url: "https://autizm6kola-art.github.io/urfinTRI/" },
   // { id: 2, name: "Гудвин", url: "https://autizm6kola-art.github.io/gudvin/" },

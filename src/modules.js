4235696fac293ed12a49b1f76b8724488ff3b25f
Светлана <autizm6kola@yandex.com>
2025-11-28 15:21:54 +0300

    какой?

--- src/modules.js
+++ src/modules.js
@@ -5,6 +5,7 @@ export const modules = [
   { id: 1, name: "Шрек", url: "https://autizm6kola-art.github.io/shrek1/" },
   { id: 2, name: "Почему?", url: "https://autizm6kola-art.github.io/istorii_pochemu/" },
   { id: 3, name: "ШРЕК", url: "https://autizm6kola-art.github.io/z-pil/" },
+  { id: 5, name: "Какой?", url: "https://autizm6kola-art.github.io/kakoy/" },
   { id: 4, name: "Панда", url: "https://autizm6kola-art.github.io/cht_panda/" },
   { id: 5, name: "Задачи 1", url: "https://autizm6kola-art.github.io/zadachi1/" },
   { id: 6, name: "Истории", url: "https://autizm6kola-art.github.io/istorii/" },

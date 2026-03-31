b0e530a207f3d082b6dcd30ce73b138eaa22b52a
Светлана <autizm6kola@yandex.com>
2026-03-31 08:19:42 +0300

    убрала вопросы и переместила чтение

--- src/modules.js
+++ src/modules.js
@@ -1,29 +1,24 @@
 export const modules = [
-  { id: 1, name: "Шрек", url: "https://autizm6kola-art.github.io/shrek1/" },
-  { id: 2, name: "Груффало", url: "https://autizm6kola-art.github.io/cht_gruffalo" },
-  { id: 3, name: "365", url: "https://autizm6kola-art.github.io/z365" },
-  { id: 4, name: "Как зовут?", url: "https://autizm6kola-art.github.io/istorii_imya/" },
-  { id: 5, name: "Словарь", url: "https://autizm6kola-art.github.io/opredelenie" },
-  { id: 6, name: "Задачи 2", url: "https://autizm6kola-art.github.io/math_1/" },
-  { id: 7, name: "ШРЕК", url: "https://autizm6kola-art.github.io/z-pil/" },
-  { id: 8, name: "Какой?", url: "https://autizm6kola-art.github.io/kakoy/" },
-  { id: 9, name: "Айболит", url: "https://autizm6kola-art.github.io/cht_aybolit" },
-  { id: 10, name: "Дочурка Груф", url: "https://autizm6kola-art.github.io/gruffalo_doch" },
-  { id: 11, name: "Хоббит", url: "https://autizm6kola-art.github.io/hobbit2/" },
-  { id: 12, name: "Почему?", url: "https://autizm6kola-art.github.io/istorii_pochemu/" },
-  { id: 13, name: "Задачи 1", url: "https://autizm6kola-art.github.io/zadachi1/" },
-  { id: 14, name: "Планка", url: "https://autizm6kola-art.github.io/sport_planka/" },
+  { id: 1, name: "Сидеть", url: "https://autizm6kola-art.github.io/p_komandi1" },
+  { id: 2, name: "Задачи 1", url: "https://autizm6kola-art.github.io/zadachi1/" },
+  { id: 3, name: "Задачи 2", url: "https://autizm6kola-art.github.io/math_1/" },
+  { id: 4, name: "Айболит", url: "https://autizm6kola-art.github.io/cht_aybolit" },
+  { id: 5, name: "Игры", url: "https://autizm6kola-art.github.io/p_igra" },
+  { id: 6, name: "Приседание", url: "https://autizm6kola-art.github.io/sport_prised" },
+  { id: 7, name: "365", url: "https://autizm6kola-art.github.io/z365" },
+  { id: 8, name: "Лис и пёс", url: "https://autizm6kola-art.github.io/lis_i_pes" },
+  { id: 9, name: "Место", url: "https://autizm6kola-art.github.io/p_mesto" },
+  { id: 10, name: "Планка", url: "https://autizm6kola-art.github.io/sport_planka/" },
+  { id: 11, name: "ШРЕК", url: "https://autizm6kola-art.github.io/z-pil/" },
+  { id: 12, name: "Груффало", url: "https://autizm6kola-art.github.io/cht_gruffalo" },
+  { id: 13, name: "Шрек", url: "https://autizm6kola-art.github.io/shrek1/" },
+  { id: 14, name: "Кольца", url: "https://autizm6kola-art.github.io/sport_kolca" },
   { id: 15, name: "Коралина", url: "https://autizm6kola-art.github.io/coralina2" },
-  { id: 16, name: "О чём думает?", url: "https://autizm6kola-art.github.io/chto_dumaet/" },
-  { id: 17, name: "Лис и пёс", url: "https://autizm6kola-art.github.io/lis_i_pes" },
-  { id: 18, name: "Игры", url: "https://autizm6kola-art.github.io/p_igra" },
-  { id: 19, name: "Кольца", url: "https://autizm6kola-art.github.io/sport_kolca" },
-  { id: 20, name: "Бокс", url: "https://autizm6kola-art.github.io/box" },
-  { id: 21, name: "Приседание", url: "https://autizm6kola-art.github.io/sport_prised" },
-  { id: 22, name: "Картошка", url: "https://autizm6kola-art.github.io/kartoshka2" },
-  { id: 23, name: "Место", url: "https://autizm6kola-art.github.io/p_mesto" },
-  { id: 24, name: "ГАРРИ", url: "https://autizm6kola-art.github.io/garry_p" },
-  { id: 25, name: "Сидеть", url: "https://autizm6kola-art.github.io/p_komandi1" }
+  { id: 16, name: "Дочурка Груф", url: "https://autizm6kola-art.github.io/gruffalo_doch" },
+  { id: 17, name: "Словарь", url: "https://autizm6kola-art.github.io/opredelenie" },
+  { id: 18, name: "Бокс", url: "https://autizm6kola-art.github.io/box" },
+  { id: 19, name: "ГАРРИ", url: "https://autizm6kola-art.github.io/garry_p" },
+  { id: 20, name: "Хоббит", url: "https://autizm6kola-art.github.io/hobbit2/" }
 ];
 
 
@@ -37,9 +32,14 @@ export const modules = [
 
 
 
+// { id: 22, name: "Картошка", url: "https://autizm6kola-art.github.io/kartoshka2" },
+// { id: 4, name: "Как зовут?", url: "https://autizm6kola-art.github.io/istorii_imya/" },
+// { id: 8, name: "Какой?", url: "https://autizm6kola-art.github.io/kakoy/" },
+// { id: 12, name: "Почему?", url: "https://autizm6kola-art.github.io/istorii_pochemu/" },
+// { id: 16, name: "О чём думает?", url: "https://autizm6kola-art.github.io/chto_dumaet/" },
    // { id: 8, name: "Истории", url: "https://autizm6kola-art.github.io/istorii/" },
   // { id: 22, name: "Муха", url: "https://autizm6kola-art.github.io/cht_muha-c" }, - для людей
-  // { id: 23, name: "Муха", url: "https://autizm6kola-art.github.io/cht_barmaley" }, - для людей
+  // { id: 23, name: "Бармалей", url: "https://autizm6kola-art.github.io/cht_barmaley" }, - для людей
   // { id: 17, name: "Рисование", url: "https://autizm6kola-art.github.io/risunki" },
   // { id: 5, name: "Урфин", url: "https://autizm6kola-art.github.io/urfinTRI/" },
   // { id: 2, name: "Гудвин", url: "https://autizm6kola-art.github.io/gudvin/" },

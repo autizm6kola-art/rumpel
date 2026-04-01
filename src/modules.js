cb066658840cac6cd9f57cfc07382fd88cd737a5
Светлана <autizm6kola@yandex.com>
2026-04-01 18:07:57 +0300

    сократила и объединила дрессировки и спорт

--- src/modules.js
+++ src/modules.js
@@ -1,24 +1,19 @@
 export const modules = [
-  { id: 1, name: "Сидеть", url: "https://autizm6kola-art.github.io/p_komandi1" },
-  { id: 2, name: "Задачи 1", url: "https://autizm6kola-art.github.io/zadachi1/" },
-  { id: 3, name: "Задачи 2", url: "https://autizm6kola-art.github.io/math_1/" },
-  { id: 4, name: "Айболит", url: "https://autizm6kola-art.github.io/cht_aybolit" },
-  { id: 5, name: "Игры", url: "https://autizm6kola-art.github.io/p_igra" },
-  { id: 6, name: "Приседание", url: "https://autizm6kola-art.github.io/sport_prised" },
-  { id: 7, name: "365", url: "https://autizm6kola-art.github.io/z365" },
-  { id: 8, name: "Лис и пёс", url: "https://autizm6kola-art.github.io/lis_i_pes" },
-  { id: 9, name: "Место", url: "https://autizm6kola-art.github.io/p_mesto" },
-  { id: 10, name: "Планка", url: "https://autizm6kola-art.github.io/sport_planka/" },
-  { id: 11, name: "ШРЕК", url: "https://autizm6kola-art.github.io/z-pil/" },
-  { id: 12, name: "Груффало", url: "https://autizm6kola-art.github.io/cht_gruffalo" },
-  { id: 13, name: "Шрек", url: "https://autizm6kola-art.github.io/shrek1/" },
-  { id: 14, name: "Кольца", url: "https://autizm6kola-art.github.io/sport_kolca" },
-  { id: 15, name: "Коралина", url: "https://autizm6kola-art.github.io/coralina2" },
-  { id: 16, name: "Дочурка Груф", url: "https://autizm6kola-art.github.io/gruffalo_doch" },
-  { id: 17, name: "Словарь", url: "https://autizm6kola-art.github.io/opredelenie" },
-  { id: 18, name: "Бокс", url: "https://autizm6kola-art.github.io/box" },
-  { id: 19, name: "ГАРРИ", url: "https://autizm6kola-art.github.io/garry_p" },
-  { id: 20, name: "Хоббит", url: "https://autizm6kola-art.github.io/hobbit2/" }
+  { id: 1, name: "Дрессировка", url: "https://autizm6kola-art.github.io/p_igra" },
+  { id: 2, name: "Спорт", url: "https://autizm6kola-art.github.io/box" },
+  { id: 3, name: "Айболит", url: "https://autizm6kola-art.github.io/cht_aybolit" },
+  { id: 4, name: "Шрек", url: "https://autizm6kola-art.github.io/shrek1/" },
+  { id: 5, name: "365", url: "https://autizm6kola-art.github.io/z365" },
+  { id: 6, name: "Лис и пёс", url: "https://autizm6kola-art.github.io/lis_i_pes" },
+  { id: 7, name: "Словарь", url: "https://autizm6kola-art.github.io/opredelenie" },
+  { id: 8, name: "ШРЕК", url: "https://autizm6kola-art.github.io/z-pil/" },
+  { id: 9, name: "Груффало", url: "https://autizm6kola-art.github.io/cht_gruffalo" },
+  { id: 10, name: "Задачи 1", url: "https://autizm6kola-art.github.io/zadachi1/" },
+  { id: 11, name: "Коралина", url: "https://autizm6kola-art.github.io/coralina2" },
+  { id: 12, name: "Дочурка Груф", url: "https://autizm6kola-art.github.io/gruffalo_doch" },
+  { id: 13, name: "Задачи 2", url: "https://autizm6kola-art.github.io/math_1/" },
+  { id: 14, name: "ГАРРИ", url: "https://autizm6kola-art.github.io/garry_p" },
+  { id: 15, name: "Хоббит", url: "https://autizm6kola-art.github.io/hobbit2/" }
 ];
 
 
@@ -32,6 +27,11 @@ export const modules = [
 
 
 
+// { id: 1, name: "Сидеть", url: "https://autizm6kola-art.github.io/p_komandi1" },
+// { id: 6, name: "Приседание", url: "https://autizm6kola-art.github.io/sport_prised" },
+// { id: 9, name: "Место", url: "https://autizm6kola-art.github.io/p_mesto" },
+// { id: 10, name: "Планка", url: "https://autizm6kola-art.github.io/sport_planka/" },
+// { id: 14, name: "Кольца", url: "https://autizm6kola-art.github.io/sport_kolca" },
 // { id: 22, name: "Картошка", url: "https://autizm6kola-art.github.io/kartoshka2" },
 // { id: 4, name: "Как зовут?", url: "https://autizm6kola-art.github.io/istorii_imya/" },
 // { id: 8, name: "Какой?", url: "https://autizm6kola-art.github.io/kakoy/" },
